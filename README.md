576901266d1f9c1bb682dc159798da7dd12a2959
Stefan Mansson <st.mansson@icloud.com>
2016-06-18 19:45:31 +0300

    Init
    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Blossom
+Blossom CSS
---
+++ blossom/js/blossom.js
@@ -0,0 +1,8 @@
+if (typeof(jQuery) == 'undefined') {
+	console.log('Blossom did not detect jQuery. Add the src to your HTML.')
+} else {
+
+		$(function() {
+				//do anything that needs to be done on document.ready
+		});
+}
