03af4d94cad57aa49de756bc6c4c375a3f9cb27a
Stefan Mansson <st.mansson@icloud.com>
2016-06-18 22:35:12 +0300

    Stages
    Finalised stages

--- blossom/js/blossom.js
+++ blossom/js/blossom.js
@@ -3,6 +3,38 @@ if (typeof(jQuery) == 'undefined') {
 } else {
 
 		$(function() {
-				//do anything that needs to be done on document.ready
+			loadProgressBars()
+			loadActiveProgressBars();
 		});
+
+		function loadProgressBars() {
+			$('.progress:not(.active) .bar[data-percent]').each(function () {
+				var progress = $(this);
+				var status = $(this).find('.status');
+				var percentage = Math.ceil($(this).attr('data-percent')) + '%';
+				progress.css('width',percentage)
+				status.text(percentage)
+			});
+		}
+
+		function loadActiveProgressBars() {
+			$('.progress.active .bar[data-percent]').each(function () {
+				var progress = $(this);
+				var status = $(this).find('.status');
+				var percentage = Math.ceil($(this).attr('data-percent'));
+				$({countNum: 0}).animate({countNum: percentage}, {
+					duration: 1600,
+					easing:'linear',
+					step: function() {
+						var pct = '';
+						if(percentage == 0){
+							pct = Math.floor(this.countNum) + '%';
+						}else{
+							pct = Math.floor(this.countNum+1) + '%';
+						}
+						status.text(pct) && progress.css('width',pct);
+					}
+				});
+			});
+		}
 }
