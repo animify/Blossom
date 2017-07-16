9f8c08f357b40f7dab5859bcfa7eb08c4ba94a9c
Stefan Mansson <st.mansson@icloud.com>
2017-07-16 18:00:52 +0300

    Update to v1.5
    Tooltips, new animations, button states, new loader, more form elements and bug fixes.

--- gulpfile.js
+++ gulpfile.js
@@ -1,3 +1,5 @@
+const fs = require('fs')
+const path = require('path')
 const gulp = require('gulp')
 const stylus = require('gulp-stylus')
 const watch = require('gulp-watch')
@@ -16,83 +18,97 @@ const jeet = require('jeet')
 const rupture = require('rupture')
 
 const source = {
-	ROOT: process.cwd() + '/../../blossom-ui',
-	ORIGIN: '.'
+  ROOT: process.cwd() + '/../../blossom-ui',
+  ORIGIN: '.'
 }
 
 let origin = source.ORIGIN
 argv.root == 1 ? origin = source.ROOT : origin = source.ORIGIN
 
 gulp.task('purge', function (cb) {
-	return del([
-		origin + '/css/blossom.min.css',
-		origin + '/js/blossom.min.js'
-	], {force: true}, cb)
+  return del([
+    origin + '/css/blossom.min.css',
+    origin + '/js/blossom.min.js'
+  ], {force: true}, cb)
 })
 
 gulp.task('watch source', function () {
-	watch(['./source/**/*.styl', './source/blossom.styl'], () => { gulp.start('compile css') })
+  watch(['./source/**/*.styl', './source/blossom.styl'], () => { gulp.start('compile css') })
 })
 
 gulp.task('compile css', function () {
-	return gulp.src('./source/blossom.styl')
-		.pipe(stylus({
-			use: [jeet(), nib(), rupture()]
-		}))
-	 .pipe(gulp.dest(origin + '/css'))
+  return gulp.src('./source/blossom.styl')
+    .pipe(stylus({
+      use: [jeet(), nib(), rupture()]
+    }))
+   .pipe(gulp.dest(origin + '/css'))
 })
 
 gulp.task('minify css', function () {
-	return gulp.src('./source/blossom.styl')
-		.pipe(stylus({
-			compress: true,
-			use: [jeet(), nib(), rupture()]
-		}))
-		.pipe(rename({
-			basename: "blossom",
-			suffix: ".min",
-		}))
-		.pipe(gulp.dest(origin + '/css'))
+  return gulp.src('./source/blossom.styl')
+    .pipe(stylus({
+      compress: true,
+      use: [jeet(), nib(), rupture()]
+    }))
+    .pipe(rename({
+      basename: "blossom",
+      suffix: ".min",
+    }))
+    .pipe(gulp.dest(origin + '/css'))
 })
 
 gulp.task('compile js', function () {
-	return gulp.src('./js/blossom.js')
-		.pipe(gulp.dest(origin + '/js'))
+  return gulp.src('./js/blossom.js')
+    .pipe(gulp.dest(origin + '/js'))
 })
 
 gulp.task('minify js', function (cb) {
-	pump([
-		gulp.src('./js/blossom.js'),
-		babel({
-			presets: ['es2015']
-		}),
-		uglify(),
-		rename({
-			basename: "blossom",
-			suffix: ".min",
-		}),
-		gulp.dest(origin + '/js')
-	], cb)
+  pump([
+    gulp.src('./js/blossom.js'),
+    babel({
+      presets: ['es2015']
+    }),
+    uglify(),
+    rename({
+      basename: "blossom",
+      suffix: ".min",
+    }),
+    gulp.dest(origin + '/js')
+  ], cb)
 })
 
 gulp.task('copy gulp', function (cb) {
-	if (origin = source.ROOT) {
-		return gulp.src('./gulpfile.js')
-			.pipe(gulp.dest(origin))
-	}
-	return false
+  if (origin = source.ROOT) {
+    return gulp.src('./gulpfile.js')
+      .pipe(gulp.dest(origin))
+  }
+  return false
 })
 
 gulp.task('copy source', function (cb) {
-	if (origin = source.ROOT) {
-		return gulp.src(['./source/**/*'])
-			.pipe(gulp.dest(origin + '/source'))
-	}
-	return false
+  if (origin = source.ROOT) {
+    return gulp.src(['./source/**/*', '!./source/**/_.styl'])
+      .pipe(gulp.dest(origin + '/source'))
+  }
+  return false
+})
+
+gulp.task('check custom', function (cb) {
+    const _path = path.join(origin, '/source/modules/_.styl')
+
+    fs.stat(_path, function(err, stat) {
+      if(err != null) {
+        return gulp.src(['./source/modules/_.styl'])
+          .pipe(gulp.dest(origin + '/source/modules'))
+      }
+
+      return false
+    })
+
 })
 
 gulp.task('build', function(cb) {
-	runSequence('purge', ['compile css', 'compile js'], ['minify css', 'minify js'], ['copy gulp', 'copy source'], cb)
+  runSequence('purge', ['compile css', 'compile js'], ['minify css', 'minify js'], ['copy gulp', 'copy source'], 'check custom', cb)
 })
 
 gulp.task('watch', ['watch source'])
--- js/blossom.js
+++ js/blossom.js
@@ -1,92 +1,92 @@
 if (typeof(jQuery) == 'undefined') {
-	console.log('Blossom did not detect jQuery. Include the latest src url at the end of your body markup.')
+  console.log('Blossom did not detect jQuery. Include the latest source url at the end of your body markup.')
 } else {
 
-		$(function() {
-			loadProgressBars()
-			loadActiveProgressBars()
-			$('.dropdown').dropdown()
+    $(function() {
+      loadProgressBars()
+      loadActiveProgressBars()
+      $('.dropdown').dropdown()
 
-			$('body').on('click', '.menu .item', function(e) {
-				const _parent = $(this).parents('.menu')
-				_parent.find('.item').removeClass('active')
-				$(this).addClass('active')
-			}).on('click', '.accordion .title', function(e) {
-				if ($(this).parent().hasClass('single') && !$(this).hasClass('open')) {
-					$('.accordion .title, .accordion .content').removeClass('open')
-					$('.accordion .content').slideUp('fast')
-				}
+      $('body').on('click', '.menu .item', function(e) {
+        const _parent = $(this).parents('.menu')
+        _parent.find('.item').removeClass('active')
+        $(this).addClass('active')
+      }).on('click', '.accordion .title', function(e) {
+        if ($(this).parent().hasClass('single') && !$(this).hasClass('open')) {
+          $('.accordion .title, .accordion .content').removeClass('open')
+          $('.accordion .content').slideUp('fast')
+        }
 
-				if ($(this).hasClass('open')) {
-					$(this).removeClass('open').next().slideUp('fast')
-				} else {
-					$(this).addClass('open').next().slideDown('fast')
-				}
-			})
-		})
+        if ($(this).hasClass('open')) {
+          $(this).removeClass('open').next().slideUp('fast')
+        } else {
+          $(this).addClass('open').next().slideDown('fast')
+        }
+      })
+    })
 
-		$.fn.extend({
-			dropdown: function(o){
-				o = $.extend({ maxHeight: 600, buffer: 100, delay: 0 }, o)
+    $.fn.extend({
+      dropdown: function(o){
+        o = $.extend({ maxHeight: 600, buffer: 100, delay: 0 }, o)
 
-				return this.each(function(){
-					var dropdown = $(this),
-						toggle = dropdown.find('.toggle'),
-						menu = dropdown.find('.menu'),
-						a = dropdown.find('a')
+        return this.each(function(){
+          var dropdown = $(this),
+            toggle = dropdown.find('.toggle'),
+            menu = dropdown.find('.menu'),
+            a = dropdown.find('a')
 
-					if(!dropdown.length) return
+          if(!dropdown.length) return
 
-					toggle.unbind("click")
-					toggle.click(function(e){
-						(!menu.is(':visible')) && $('.menu').parent().removeClass('open')
+          toggle.unbind("click")
+          toggle.click(function(e){
+            (!menu.is(':visible')) && $('.menu').parent().removeClass('open')
 
-						setTimeout(function() {
-							menu.parent().addClass('open')
-						}, o.delay)
-					})
+            setTimeout(function() {
+              menu.parent().addClass('open')
+            }, o.delay)
+          })
 
-					a.bind('click', function(e){
-						menu.parent().removeClass('open')
-					})
+          a.bind('click', function(e){
+            menu.parent().removeClass('open')
+          })
 
-					$(document).bind('click', function(e) {
-						if (! $(e.target).parents().hasClass('dropdown'))
-							menu.parent().removeClass('open')
-					 })
-				})
-			}
-		})
+          $(document).bind('click', function(e) {
+            if (! $(e.target).parents().hasClass('dropdown'))
+              menu.parent().removeClass('open')
+           })
+        })
+      }
+    })
 
 
-		function loadProgressBars() {
-			$('.progress:not(.active) .bar[data-percent]').each(function () {
-				const progress = $(this)
-				const status = $(this).find('.status')
-				const percentage = Math.ceil($(this).attr('data-percent')) + '%'
-				progress.css('width',percentage)
-				status.text(percentage)
-			})
-		}
+    function loadProgressBars() {
+      $('.progress:not(.active) .bar[data-percent]').each(function () {
+        const progress = $(this)
+        const status = $(this).find('.status')
+        const percentage = Math.ceil($(this).attr('data-percent')) + '%'
+        progress.css('width',percentage)
+        status.text(percentage)
+      })
+    }
 
-		function loadActiveProgressBars() {
-			$('.progress.active .bar[data-percent]').each(function () {
-				const progress = $(this)
-				const status = $(this).find('.status')
-				const percentage = Math.ceil($(this).attr('data-percent'))
-				$({countNum: 0}).animate({countNum: percentage}, {
-					duration: 1600,
-					easing:'linear',
-					step: function() {
-						var pct = ''
-						if(percentage == 0){
-							pct = Math.floor(this.countNum) + '%'
-						}else{
-							pct = Math.floor(this.countNum+1) + '%'
-						}
-						status.text(pct) && progress.css('width',pct)
-					}
-				})
-			})
-		}
+    function loadActiveProgressBars() {
+      $('.progress.active .bar[data-percent]').each(function () {
+        const progress = $(this)
+        const status = $(this).find('.status')
+        const percentage = Math.ceil($(this).attr('data-percent'))
+        $({countNum: 0}).animate({countNum: percentage}, {
+          duration: 1600,
+          easing:'linear',
+          step: function() {
+            var pct = ''
+            if(percentage == 0){
+              pct = Math.floor(this.countNum) + '%'
+            }else{
+              pct = Math.floor(this.countNum+1) + '%'
+            }
+            status.text(pct) && progress.css('width',pct)
+          }
+        })
+      })
+    }
 }
