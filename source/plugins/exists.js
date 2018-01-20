dfa4fbcc9c64c9545759ffe74518c7d76bc26095
Stefan Mansson <stefan.aotik@gmail.com>
2018-01-20 17:06:14 +0200

    style: Variable restructure & stylint added

--- source/plugins/exists.js
+++ source/plugins/exists.js
@@ -2,8 +2,11 @@ const stylus = require('stylus');
 
 module.exports = function() {
     return function(style) {
-        style.define('exists', function(path) {
-            return !!stylus.utils.lookup(path.string + '.styl', this.paths);
+        style.define('ifExists', function(path, theme) {
+            const pathToThemes = './../themes/' + theme + '/' + path + '/style';
+            const pathName = !!stylus.utils.lookup(pathToThemes.string + '.styl', this.paths);
+            console.log(pathName);
+            stylus(path).import(pathName);
         });
     };
 };
